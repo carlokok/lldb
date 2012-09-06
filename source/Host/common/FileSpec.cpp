8274d03f75eba1d782b9ebe20582e2699eec5627
Carlo Kok <ck@remobjects.com>
2012-09-06 14:27:26 +0200

    Make regular expressions compile with the new lldb.

--- source/Host/common/FileSpec.cpp
+++ source/Host/common/FileSpec.cpp
@@ -1003,7 +1003,7 @@ FileSpec::IsSourceImplementationFile () const
     if (extension)
     {
         static RegularExpression g_source_file_regex ("^(c|m|mm|cpp|c\\+\\+|cxx|cc|cp|s|asm|f|f77|f90|f95|f03|for|ftn|fpp|ada|adb|ads)$",
-                                                      REG_EXTENDED | REG_ICASE);
+                                                      llvm::Regex::IgnoreCase);
         return g_source_file_regex.Execute (extension.GetCString());
     }
     return false;
